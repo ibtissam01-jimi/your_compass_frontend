4493a660c6f49f6f77ec9af244a5208383e74904
ameurelmoukh04 <ameurelmoukh04@gmail.com>
2025-04-18 19:38:09 +0100

    commited

--- src/components/about/categories.jsx
+++ src/components/about/categories.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ExperienceGrid() {
+
+const [categories,setCategories] = useState([]);
+  useEffect(()=>{
+    const fetchCategories =()=>{
+
+      const BACKEND_URL='http://localhost:8000/api/categories';
+      axios.get(BACKEND_URL)
+      .then((response)=>{
+        console.log(response.data)
+        setCategories(response.data)
+      })
+    }
+    fetchCategories();
+  },[])
     const data = [
       { title: "Amazing Views", img: "/images/views.jpg" },
       { title: "Best Moments", img: "/images/moments.jpg" },
@@ -11,14 +28,14 @@ export default function ExperienceGrid() {
     return (
       <div className="py-16 px-4 bg-white">
         <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {data.map((item, index) => (
+          {categories.map((item, index) => (
             <div
               key={index}
               className="relative rounded-xl overflow-hidden shadow-lg group h-80 cursor-pointer"
             >
               <img
                 src={item.img}
-                alt={item.title}
+                alt={item.name}
                 className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
               />
               <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
--- src/components/forms/addBusiness.jsx
+++ src/components/forms/addBusiness.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from "react-router-dom";
 
 const BusinessForm = () => {
+  const [categories,setCategories] = useState([]);
+  const [cities,setCities] = useState([]);
+  const token = localStorage.getItem('token');
+
+
+  const {register,handleSubmit,formState: { errors },} = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const BACKEND_URL='http://localhost:8000/api/new_submission';
+    console.log(token)
+    const config = {
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${token}`,
+      }
+  }
+    console.log(data);
+    const response = await axios.post(BACKEND_URL, data,config);
+    try {
+      if (response.status === 200) {
+        alert('we receive your submissions we will review it in next days');
+      }
+    } catch (error) {
+      alert("data not valid");
+      console.log("data not valid");
+    }
+  };
+
+  useEffect(()=>{
+    const fetchCities =()=>{
+      
+      const BACKEND_URL='http://localhost:8000/api/cities';
+      axios.get(BACKEND_URL)
+      .then((response)=>{
+        console.log(response.data)
+        setCities(response.data)
+      })
+    }
+    fetchCities();
+  },[])
+    useEffect(()=>{
+      const fetchCategories =()=>{
+        const BACKEND_URL='http://localhost:8000/api/categories';
+  
+        axios.get(BACKEND_URL)
+        .then((response)=>{
+          console.log(response.data)
+          setCategories(response.data)
+        })
+
+        
+      }
+      
+      fetchCategories();
+    },[])
+
+
   return (
     <div
       className="min-h-screen bg-cover bg-center flex items-center justify-center"
@@ -10,7 +71,7 @@ const BusinessForm = () => {
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/images/formbuss.jpg")',
       }}
     >
-      <form
+      <form onSubmit={handleSubmit(onSubmit)}
         className="bg-[#1B1F26] bg-opacity-0 p-8 md:p-12 rounded-lg w-full max-w-3xl text-white space-y-6"
         style={{
           backgroundColor: 'rgba(29, 35, 39, 0.6)', 
@@ -41,7 +102,7 @@ const BusinessForm = () => {
         {/* Business Name */}
         <div>
           <label className="block text-sm capitalize">Business Name</label>
-          <input
+          <input {...register('name')}
             type="text"
             className="w-full p-2 rounded border border-gray-400 bg-transparent text-white"
             placeholder="Enter business name"
@@ -51,19 +112,20 @@ const BusinessForm = () => {
         {/* Category */}
         <div>
           <label className="block text-sm capitalize">Select Category</label>
-          <select
-            className="w-full p-2 rounded border border-gray-400 bg-transparent text-white"
+          <select {...register('category_id')}
+            className="w-full p-2 rounded border border-gray-400 text-black"
           >
-            <option value="">-- Select --</option>
-            <option>Restaurant</option>
-            <option>Shop</option>
+            
+            {categories.map((item)=>(
+              <option value={item.id}>{item.name}</option>
+            ))}
           </select>
         </div>
 
         {/* Description */}
         <div>
           <label className="block text-sm capitalize">Business Description</label>
-          <textarea
+          <textarea {...register('description')}
             className="w-full p-2 rounded border border-gray-400 bg-transparent text-white"
             placeholder="Description"
             rows="3"
@@ -82,21 +144,20 @@ const BusinessForm = () => {
         {/* Address */}
         <div>
           <label className="block text-sm capitalize">Address</label>
-          <input
+          <input {...register('address')}
             type="text"
             className="w-full p-2 rounded mb-2 border border-gray-400 bg-transparent text-white"
             placeholder="Street address"
           />
           <div className="flex space-x-2">
-            <input
-              type="text"
-              className="w-1/2 p-2 rounded border border-gray-400 bg-transparent text-white"
-              placeholder="City"
-            />
+          <select {...register('city_id')} className="w-1/2 p-2 rounded border border-gray-400 bg-transparent text-black">
+          
+          {cities.map((item)=>(
+              <option value={item.id}>{item.name}</option>
+            ))}
+            </select>
             <select className="w-1/2 p-2 rounded border border-gray-400 bg-transparent text-white">
               <option>Morocco</option>
-              <option>France</option>
-              <option>USA</option>
             </select>
           </div>
         </div>
@@ -104,19 +165,24 @@ const BusinessForm = () => {
         {/* Business Contact */}
         <div>
           <h3 className="font-semibold mt-4 capitalize">Business Contact</h3>
-          <input
+          <input {...register('website')}
+            type="website"
+            className="w-full p-2 rounded mb-2 border border-gray-400 bg-transparent text-white"
+            placeholder="Business Website"
+          />
+          <input {...register('email')}
             type="email"
             className="w-full p-2 rounded mb-2 border border-gray-400 bg-transparent text-white"
             placeholder="Email Address"
           />
-          <input
+          <input {...register('phone_number')}
             type="tel"
             className="w-full p-2 rounded border border-gray-400 bg-transparent text-white"
             placeholder="Phone number"
           />
         </div>
 
-        {/* Manager Contact */}
+        {/* Manager Contact
         <div>
           <h3 className="font-semibold mt-4 capitalize">Manager Contact</h3>
           <input
@@ -129,7 +195,7 @@ const BusinessForm = () => {
             className="w-full p-2 rounded border border-gray-400 bg-transparent text-white"
             placeholder="Mobile"
           />
-        </div>
+        </div> */}
 
         {/* Buttons */}
         <div className="flex justify-between mt-6">
